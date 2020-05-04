06b61a6462e79554f2bcbd3308ef79afcc5d9e9f
pamalmangat <pamal13@hotmail.com>
2020-05-04 11:11:06 -0700

    - Testing out API GET request to fetch data.

--- displayData.py
+++ displayData.py
@@ -4,5 +4,13 @@
 #For reference, please remember that the countries are associated with an integer value, that is specific to each country.
 #Where, [USA = 0, CANADA = 1, FRANCE = 2, U.K = 3, SPAIN = 4, ITALY = 5, WORLDWIDE = 6]
 
+import main
+import requests
+
 def display(countryCode):
-    print(countryCode)
+    response = requests.get("https://api.covid19api.com/live/country/canada")
+    print(response.text.encode('utf8'))
+
+
+#For testing purposes just run data for U.S.A currently.
+display(0)
