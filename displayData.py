f669abd2f2ea4b58effce685214a0e4dcf82418c
pamalmangat <pamal13@hotmail.com>
2020-05-04 15:43:22 -0700

    -Removed some unnessary data structures. -Will be directly passing country slug values instead of integer values between .py files

--- displayData.py
+++ displayData.py
@@ -1,23 +1,11 @@
 #This (.py) handles the display of data.
 #When user makes a selection on the main.py, they are directed to this script upon the ['Next'] button click.
 
-#For reference, please remember that the countries are associated with an integer value, that is specific to each country.
-#Where, [USA = 0, CANADA = 1, FRANCE = 2, U.K = 3, SPAIN = 4, ITALY = 5, WORLDWIDE = 6]
-
-import main
 import requests
 
 #Each country is associated with it's own API request url.
 
-#The API documentation for Postman has different 'slug' urls for each country. 
-countrySlug = {0: "united-states",
-               1: "canada",
-               2: "france",
-               3: "united-kingdom",
-               4: "spain",
-               5: "italy"}
-
-def display(countryCode):
-    response = requests.get("https://api.covid19api.com/live/country/" + countrySlug.get(countryCode))
+def display(countrySlug):
+    response = requests.get("https://api.covid19api.com/live/country/" + countrySlug)
     print(response.text.encode('utf8'))
 
